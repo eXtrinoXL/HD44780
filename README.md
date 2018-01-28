db209d3ee24e34112b6644630f2e89b67df4afd4
Pawel 'felixd' Wojciechowski <felixd@konopnickiej.com>
2018-01-28 15:47:30 +0100

    Lib init

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# HD44780
-HD44780 library for eXtrinoXL FULL board.
+# eXtrinoXL-HD44780
+Biblioteka obsługująca wyświetlacz alfanumeryczny ze sterownikiem HD44780. Sterowanie odbywa się w trybie 4-bitowym, bez odczytu flagi zajętości z dowolnym przypisaniem sygnałów sterujących.
+
+Biblioteka przygotowana jest do obsługi mikrokontrolera Atmel AVR XMEGA128A3U znajdującego się na płytce eXtrino XL FULL wyprodukowanej przez Leon Instruments.
+
+## Źródła
+* [Kurs na stronie Leon Instruments](https://extronic.pl/content/48-kurs-xmega-wyswietlacz-lcd)
+* [Oryginalna biblioteka HD44780 autorstwa Radosława Kwietnia](http://radzio.dxp.pl)
+
+## Autorzy
+* Radosław Kwiecień - <http://radzio.dxp.pl>
+* Dominik Bieczyński (Leon Instruments) - <https://extronic.pl>
+* Paweł 'felixd' Wojciechowski - <http://www.konopnickiej.com>
+
+## Narzędzia
+ * Edytor: [Visual Studio Code](https://code.visualstudio.com/) + [PlatformioIO](http://platformio.org/)
+ * Formatowanie kodu: **clang-format**
+ * Kodowanie: **UTF-8**
+ * Znak końca linii: **Unix LF**
+
+## Uwagi do Platformio IO / VSCode
+
+### IntelliSense i <avr/io.h> a xmega128a3u
+W celu poprawnego działania **IntelliSense** w **VSCode** upewnij się, że plik `.vscode/c_cpp_properties.json` w sekcji `defines` ma dokładnie tak samo określony rodzaj mikrokontrolera:
+
+```
+            "defines": [
+                "__AVR_ATxmega128A3U__",
+```
+
+Więcej informacji pod linkiem: https://github.com/Microsoft/vscode-cpptools/issues/690
---
+++ examples/main.c
@@ -0,0 +1,65 @@
+/*
+  Dominik Leon Bieczyński
+  Leon Instruments
+  http://leon-instruments.blogspot.com
+
+  Data: 24.01.2018
+  Dostowanie do płytki eXtrinoXL FULL [Atmel AVR XMEGA128A3U]
+  Paweł 'felixd' Wojciechowski
+  Outsourcing IT - Konopnickiej.Com
+  http://www.konopnickiej.com
+
+     +----------------+
+LCD	 |1234567890123456|
+LCD2 |1234567890123456|
+     +----------------+
+
+W naszym przykładzie po uruchomieniu ekran wyglądać będzie następująco:
+
+       +----------------+
+	LCD	 |CPU: 2000000 Hz |
+	LCD2 |Konopnickiej.Com|
+       +----------------+
+			 
+*/
+
+#define F_CPU 2000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+#include <hd44780.h>
+
+int main(void)
+{
+	// Konfiguracja przycisku FLIP
+	PORTE.DIRCLR = PIN5_bm;				 // pin E5 jako wejście
+	PORTE.PIN5CTRL = PORT_OPC_PULLUP_gc; // podciągnięcie do zasilania
+
+	// Inicjalizacja sterownika LCD
+	LcdInit();
+
+	// Tworzenie pierwszego wiersza
+	Lcd("CPU: ");
+	LcdDec(F_CPU);
+	Lcd(" Hz");
+
+	// Przejście do drugiej linii
+	Lcd2;
+	// Tworzenie drugiego wiersza
+	Lcd("Konopnickiej Com");
+
+	// Deklarujemy zmienną służącą jako licznik
+	uint8_t licznik = 0;
+
+	while (1)
+	{
+		if (!(PORTE.IN & PIN5_bm))
+		{					 // jeżeli przycisk FLIP jest wciśnięty
+			licznik++;		 // zwiększ licznik o 1
+			LcdClear();		 // wyczyśc wyświetlacz
+			Lcd("Licznik:"); // wyświetlenie napisu
+			Lcd2;			 // przejście do drugiej linii
+			LcdDec(licznik); // wyświetlenie zmiennej liczbowej
+			_delay_ms(100);  // czekanie 100ms
+		}
+	}
+}
